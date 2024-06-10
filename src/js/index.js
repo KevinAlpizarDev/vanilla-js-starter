2af6218304ad305af1c381e2838678e1d7283c9b
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-06-09 22:56:48 -0600

    code identification

--- src/js/index.js
+++ src/js/index.js
@@ -2,117 +2,67 @@
 import { postTask, getTask, deleteTask, putTask } from "./fetch";
 import { myFunctionsObject } from './module';
 
+// SELECTION OF HTML ELEMENTS
+// ELEMENTOS DE LA APLICACION PRINCIPAL
+const input = document.querySelector("input"); // INPUT DE INGRESO DE TAREAS
+const addBtn = document.querySelector(".btn-add"); // BOTON PARA AÑADIR TAREA
+const ul = document.querySelector("ul"); // LISTA DONDE SE AGREGAN LOS ELEMENTOS 
+const empty = document.querySelector(".empty"); // PARRAFO CUANDO NO HAY TAREAS
 
-//SELECTION OF HTML ELEMENTS
-
-//ELEMENTOS DE LA APLICACION PRINCIPAL
-const input = document.querySelector("input");//INPUT DE INGRESO DE TAREAS
-const addBtn = document.querySelector(".btn-add");//BOTON PARA AÑADIR TAREA
-const ul = document.querySelector("ul");//LISTA DONDE SE AGREGAN LOS ELEMENTOS 
-const empty = document.querySelector(".empty");//PARRAFO CUANDO NO HAY TAREAS
-
-//ELEMENTOS DEL MODAL 
-// const overlay = document.querySelector('.overlay');
-
+// ELEMENTOS DEL MODAL 
 const btnsOpenModal = document.querySelector('.show-modal');
-// ////////////////////////////////////////////////////////////////////
 
-//GET FUNCTION
+// GET FUNCTION
 async function cargarTareas() {
-    let tareasPrometidas = await getTask()
-    empty.style.display = tareasPrometidas.length <= 0 ? "block" : "none"; //Cambia el estilo del display 
+    let tareasPrometidas = await getTask();
+    empty.style.display = tareasPrometidas.length <= 0 ? "block" : "none"; // Cambia el estilo del display 
 
     for (let i = 0; i < tareasPrometidas.length; i++) {
-        const li = document.createElement("li")
+        const li = document.createElement("li");
         const p = document.createElement("p");
         p.textContent = tareasPrometidas[i].task;
         li.id = tareasPrometidas[i].id;
-        //FUNCION IMPORTADA
-        myFunctionsObject.tasksCounter()//CUENTA LA CANTIDAD DE TAREAS 
-    ClickValidate(li)
-        // validaClick(li)
-         li.appendChild(p)
+        // FUNCION IMPORTADA
+        myFunctionsObject.tasksCounter(); // CUENTA LA CANTIDAD DE TAREAS 
+        ClickValidate(li);
+        li.appendChild(p);
         ul.appendChild(li);
-        //CREATE CHECKBOX FUNCTION IMPORTED
+        // CREATE CHECKBOX FUNCTION IMPORTED
         li.appendChild(myFunctionsObject.addDeleteBtn());
     }
-    llamarBorrado()
+    myFunctionsObject.llamarBorrado();
 }
 
-
-cargarTareas()
-
-
-async function llamarBorrado() {
-    let tareasPrometidas = await getTask()
-
-    for (let i = 0; i < tareasPrometidas.length; i++) {
-        const li = document.createElement("li")
-        const p = document.createElement("p");
-        li.id = tareasPrometidas[i].id
-    }
-}
+cargarTareas();
 
 function ClickValidate(li) {
     li.addEventListener("change", function () {
-        //SETTIMEOUT PARA DELETE DEL API
+        // SETTIMEOUT PARA DELETE DEL API
         setTimeout(() => {
-            deleteTask(li.id)
-            //TRES SEGUNDOS DE RETARDO
+            deleteTask(li.id);
+            // TRES SEGUNDOS DE RETARDO
         }, 1000);
-    })
+    });
 }
 
-
 document.addEventListener('keydown', function (e) {
-e.key === 'Escape' || input.value != "" ? postTask(input.value) : console.log(3);
-})
+    e.key === 'Escape' || input.value != "" ? postTask(input.value) : console.log(3);
+});
 
-//ESCUCHA AL BOTON AGREGAR TAREAS
+// ESCUCHA AL BOTON AGREGAR TAREAS
 addBtn.addEventListener("click", function () {
-//VALIDACION TAREAS VACÍAS
-input.value.trim() != "" ?    postTask(input.value) :   alert("tareas vacías no es valido") 
-})
-
+    // VALIDACION TAREAS VACÍAS
+    input.value.trim() != "" ? postTask(input.value) : alert("Write a Task");
+});
 
 // FUNCION IMPORTADA
-myFunctionsObject.currentDay()//MUESTRA EL DÍA ACTUAL 
-myFunctionsObject.openModal()
-
-    // MODAL LOGIC
-    // const openModal = function () {//ACCION AL ABRIR EL MODAL 
-    //   overlay.classList.remove('hidden');//
-    // };
-
-    // openModal()
-
-//MODAL LOGIC
-// const closeModal = function () { //ACCION AL CERRAR EL MODAL 
-//   overlay.classList.add('hidden');
-// }
-
+myFunctionsObject.currentDay(); // MUESTRA EL DÍA ACTUAL 
 
 // EVENTO DE ESCUCHA PARA CERRAR EL MODAL
 btnsOpenModal.addEventListener("click", function () {
-    // closeModal()
-    myFunctionsObject.closeModal()
-    })
-    
-//////////////////////
-
-function calcular(operacion, numero1, numero2) { //Parametro 
-    switch (operacion) {
-    case "sumar" :
-        return myFunctionsObject.sumar(numero1, numero2) 
-    }
-}
-export { calcular}
-
-console.log(calcular("sumar", 1,2));
-
-
-
-
+  
+    myFunctionsObject.closeModal();
+});
 
 
 
--- src/js/module.js
+++ src/js/module.js
@@ -1,98 +1,63 @@
-//VARIALES GLOBALES
+// VARIALES GLOBALES
 let pendingTaskCounter = 0;
 
 export const myFunctionsObject = {
-// FIRST FUNCTION
-  tasksCounter: function() {
+  // FIRST FUNCTION
+  tasksCounter: function () {
     // Incrementa el contador cada vez que se llama a la función
     pendingTaskCounter++;
     let counter = document.getElementById("counter");
     counter.innerHTML = pendingTaskCounter;
   },
-// SECOND FUNCTION
+  // SECOND FUNCTION
   currentDay: function () {
-    
     let today = document.getElementById("today")
-    
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
     const d = new Date();
     console.log(d);
     let weekday = days[d.getDay()];
     const dayOfMonth = d.getDate();
     console.log(dayOfMonth);
-    
     today.innerHTML = `Tasks for: ${weekday}`
-
-
   },
 
   // SUMA
+  sumar: function (a, b) {
+    return a + b
+  },
 
-sumar: function (a,b) {
-  return a + b
-},
+  /////////////// MODAL FUNCTIONS
 
-/////////////// MODAL FUNCTIONS
+  ///////////////OPEN MODAL FUNCTIONS 
+  openModal: function () {
+    const overlay = document.querySelector('.overlay')
+    overlay.classList.remove("hidden")
+  },
 
-///////////////OPEN MODAL FUNCTIONS 
-openModal: function  () {
-  const overlay = document.querySelector('.overlay')
-  overlay.classList.remove("hidden")
-},
+  ///////////////CLOSE MODAL FUNCTIONS 
+  closeModal: function () { //ACCION AL CERRAR EL MODAL 
+    const overlay = document.querySelector('.overlay')
+    overlay.classList.add('hidden');
+  },
 
-
-///////////////CLOSE MODAL FUNCTIONS 
-
-closeModal: function () { //ACCION AL CERRAR EL MODAL 
-  const overlay = document.querySelector('.overlay')
-  overlay.classList.add('hidden');
-}, 
-
-
-////////////////CREATE CHECKBOX DIMAMICLY 
-addDeleteBtn: function () {
-
+  ////////////////CREATE CHECKBOX DIMAMICLY 
+  addDeleteBtn: function () {
     const deleteBtn = document.createElement("input");
     deleteBtn.type = "checkbox";
     deleteBtn.id = "eliminar";
     deleteBtn.className = "some-name"; // AGREGARLE UNA CLASE
-    return deleteBtn;//RETORNA EL CHECKBOX
+    return deleteBtn; //RETORNA EL CHECKBOX
+  },
 
-}
-
-// ,
-// //CLICK VALIDATE A CHECKBOX CHANGE
-// ClickValidate: function (li) {
-//       // SETTIMEOUT PARA DELETE DEL API
-//         setTimeout(() => {
-//             deleteTask(li.id)
-//             //TRES SEGUNDOS DE RETARDO
-//         }, 1000);
-// }
-
-
-// function ClickValidate(li) {
-//     li.addEventListener("change", function () {
-//         //SETTIMEOUT PARA DELETE DEL API
-//         setTimeout(() => {
-//             deleteTask(li.id)
-//             //TRES SEGUNDOS DE RETARDO
-//         }, 1000);
-//     })
-// }
-
-
-// const openModal = function () {//ACCION AL ABRIR EL MODAL 
-//   overlay.classList.remove('hidden');//
-// };
-
-
-
+  // GETTASK ASYNC FUNCTION
+  async llamarBorrado() {
+    let tareasPrometidas = await getTask()
+    for (let i = 0; i < tareasPrometidas.length; i++) {
+      const li = document.createElement("li")
+      const p = document.createElement("p");
+      li.id = tareasPrometidas[i].id
+    }
+  }
 };
 
-
-
-
-
-exports.myFunctionsObject = myFunctionsObject
+exports.myFunctionsObject = myFunctionsObject;
